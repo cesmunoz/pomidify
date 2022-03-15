91a74502f46a6463d0ee8def554b8899bdf3c3f7
Cesar Munoz <cesar.alberto.munoz@gmail.com>
2022-03-15 23:57:53 +0100

    Adding turborepo - nextjs - tailwindcss

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ui";
+
+export default function Web() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Web</h1>
+      <Button />
+    </div>
+  );
+}
